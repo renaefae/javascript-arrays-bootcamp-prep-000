dcf6b7e4c4eff5572050467ad20c0ee44ae22a9c
Naenae L <oneprincesslea@gmail.com>
2018-11-13 00:53:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,2 @@
 var chocolateBars = [godiva, hershey, moonstruck] ;
-console.log(chocolateBars);
-chocolateBars.push("bridgewater");
-console.log (chocolateBars);
-chocolateBars.unshift("chocolove");
-["devries", ...chocolateBars];
-console.log(chocolateBars);
+
